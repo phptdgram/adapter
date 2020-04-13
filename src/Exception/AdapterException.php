8f57e2900452d3c130bb0dff1e92c9d516133804
Aurimas Niekis <aurimas@niekis.lt>
2020-04-13 13:08:36 +0900

    Initial Commit

---
+++ src/Exception/AdapterException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPTdGram\Adapter\Exception;
+
+use Exception;
+
+/**
+ * @author  Aurimas Niekis <aurimas@niekis.lt>
+ */
+class AdapterException extends Exception
+{
+}
